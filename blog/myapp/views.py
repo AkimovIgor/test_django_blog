e6777104166d5299bdd8c19e4344b4d5976e0466
AkimovIgor <akimovigor5417@gmail.com>
2022-08-16 18:47:41 +0300

    add pagination

--- blog/myapp/views.py
+++ blog/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.core.paginator import Paginator
 from .models import Post
 
 
@@ -7,7 +8,10 @@ class MainView(View):
 
     @staticmethod
     def get(request, *args, **kwargs):
-        posts = Post.objects.all()
+        posts_list = Post.objects.all().order_by('-id')
+        paginator = Paginator(posts_list, 6)
+        posts = paginator.get_page(request.GET.get('page', 1))
         return render(request, 'myapp/pages/home.html', context={
-            'posts': posts
+            'posts': posts,
+            'pages': range(1, 4)
         })
